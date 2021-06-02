536577733a6d7357874058fb36d41b272e9a6a61
Henrique0Mendes <henriquemendes1711@gmail.com>
2021-06-02 15:29:21 +0100

    3 commit

--- projFinal/src/app/app.module.ts
+++ projFinal/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JogoComponent } from './blackJack/components/jogo/jogo.component';
+import { HomeComponent } from './components/home/home.component';
+import { FourOFourComponent } from './components/four-ofour/four-ofour.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JogoComponent,
+    HomeComponent,
+    FourOFourComponent,
   ],
   imports: [
     BrowserModule,
