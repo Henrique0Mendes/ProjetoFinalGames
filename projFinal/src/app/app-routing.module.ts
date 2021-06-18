7df2d6f25e4b429208a8ba04b860fa07589fd247
Mateus Oliveira <mateusamaraloliveira160302@gmail.com>
2021-06-18 16:32:51 +0100

    começar o 404

--- projFinal/src/app/app-routing.module.ts
+++ projFinal/src/app/app-routing.module.ts
@@ -4,14 +4,18 @@ import { FourOFourComponent } from './components/four-ofour/four-ofour.component
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:"", component:HomeComponent},
-  {path:"home", component:HomeComponent},
-  {path:"notFound", component:FourOFourComponent},
-  {path:"**",redirectTo: "notFound"},
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'notFound', component: FourOFourComponent },
+  { path: '**', redirectTo: 'notFound' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
