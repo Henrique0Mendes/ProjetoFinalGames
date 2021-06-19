1d093cd3868036e549ea81e0dac2b659c97a2cf7
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2021-06-19 23:49:27 +0100

    home feito

--- projFinal/src/app/components/home/home.component.ts
+++ projFinal/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(router: Router) {
+    this.router = router;
+  }
 
   ngOnInit(): void {
   }
 
+  // vars
+  router: Router;
+
+  growUp(element: HTMLElement, value: number) {
+    element.classList.toggle("growUp");
+
+    setTimeout(() => {
+      element.classList.toggle("growUp");
+
+      setTimeout(() => {
+        switch (value) {
+          case 1:
+            this.router.navigate(['/blackjack']);
+            break;
+
+          case 2:
+            this.router.navigate(['/cripto']);
+            break;
+
+          default:
+            break;
+        }
+      }, 350);
+    }, 400);
+  }
+
+  blackJChosen(element: HTMLElement) {
+    return this.growUp(element, 1);
+  }
+
+  criptoChosen(element: HTMLElement) {
+    return this.growUp(element, 2);
+  }
 }
